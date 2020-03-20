d8b91b7407b2a07c64f82d1c1c08ce5d55a5ed52
arenchilingaryan <58465410+arenchilingaryan@users.noreply.github.com>
2020-03-20 16:00:48 +0300

    Updates

---
+++ precache-manifest.95b1fa63dd53b0c86422a732870e0736.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4f2c5fa5251e98d14623bbd881ba18fe",
+    "url": "/films/index.html"
+  },
+  {
+    "revision": "75a0de4bb697faa1dc04",
+    "url": "/films/static/css/main.43ab5ecb.chunk.css"
+  },
+  {
+    "revision": "b302d06ba5cc96c55a8f",
+    "url": "/films/static/js/2.a3b2947a.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/films/static/js/2.a3b2947a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "75a0de4bb697faa1dc04",
+    "url": "/films/static/js/main.5ee5c253.chunk.js"
+  },
+  {
+    "revision": "19ddddc90e326b4e5a8f",
+    "url": "/films/static/js/runtime-main.f6ce3f63.js"
+  }
+]);
